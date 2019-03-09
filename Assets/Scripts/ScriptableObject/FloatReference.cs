5f4eaf019488072dcd7d24a3399d2f1dfe7f7dae
Mohamad Rashad <m.rashad.sm@gmail.com>
2019-03-09 19:36:14 +0200

    Create FloatVariable Scriptable Object and create asset for Speed Value Create FloatRefrence Scriptable Object for Designers to help them choose speed value from constant value or from shared Scriptable Object and also create Assets for two player Create  Player Scriptable Object to store player data and variables

---
+++ Assets/Scripts/ScriptableObject/FloatReference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class FloatRefrence
+{
+    public bool UseConstant = false;
+    public float ConstantValue = 0f;
+    public FloatVariable Variable;
+    public float Value
+    {
+        get { return UseConstant ? ConstantValue : Variable.Value; }
+    }
+}
