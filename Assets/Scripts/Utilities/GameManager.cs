b2d846908b7e8c8c1cb3f964ad4d16287db8b32b
Mohamad Rashad <m.rashad.sm@gmail.com>
2019-03-09 19:37:20 +0200

    Create Input Handler To handling Inputs Commands Create Game Manager To handling data between objects in Sceen

---
+++ Assets/Scripts/Utilities/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    PlayerSP playerBlue;
+    [SerializeField]
+    PlayerSP playerRed;
+    [SerializeField]
+    GameObject playerBlueGameObject;
+    [SerializeField]
+    GameObject playerRedGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        playerBlue.PlayerObject = playerBlueGameObject;
+        playerRed.PlayerObject = playerRedGameObject;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Utilities/InputHandler.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField]
+    PlayerSP playerBlue;
+    [SerializeField]
+    PlayerSP playerRed;
+
+    Rigidbody2D playerBody;
+    //Player  Movement Buttons
+    private Command buttonW, buttonS, buttonA, buttonD;
+    private Command buttonI, buttonK, buttonJ, buttonL;
+
+    void Start()
+    {
+        //Bind keys with commands
+        buttonW = new MovingCommand(playerBlue.PlayerObject, playerBlue.Speed,MovingDirction.Up);
+        buttonS = new MovingCommand(playerBlue.PlayerObject, playerBlue.Speed, MovingDirction.Down);
+        buttonA = new MovingCommand(playerBlue.PlayerObject, playerBlue.Speed, MovingDirction.Left);
+        buttonD = new MovingCommand(playerBlue.PlayerObject, playerBlue.Speed, MovingDirction.Right);
+
+        buttonI = new MovingCommand(playerRed.PlayerObject, playerRed.Speed, MovingDirction.Up);
+        buttonK = new MovingCommand(playerRed.PlayerObject, playerRed.Speed, MovingDirction.Down);
+        buttonJ = new MovingCommand(playerRed.PlayerObject, playerRed.Speed, MovingDirction.Left);
+        buttonL = new MovingCommand(playerRed.PlayerObject, playerRed.Speed, MovingDirction.Right);
+
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            buttonA.Execute();
+        }
+        else if(Input.GetKeyUp(KeyCode.A))
+        {
+            buttonA.StopExecuting();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            buttonD.Execute();
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            buttonD.StopExecuting();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            buttonS.Execute();
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            buttonS.StopExecuting();
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            buttonW.Execute();
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            buttonW.StopExecuting();
+        }
+
+
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            buttonJ.Execute();
+        }
+        else if (Input.GetKeyUp(KeyCode.J))
+        {
+            buttonJ.StopExecuting();
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            buttonL.Execute();
+        }
+        else if (Input.GetKeyUp(KeyCode.L))
+        {
+            buttonL.StopExecuting();
+        }
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            buttonK.Execute();
+        }
+        else if (Input.GetKeyUp(KeyCode.K))
+        {
+            buttonK.StopExecuting();
+        }
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            buttonI.Execute();
+        }
+        else if (Input.GetKeyUp(KeyCode.I))
+        {
+            buttonI.StopExecuting();
+        }
+    }
+}
