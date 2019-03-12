886de4b6c2393556893e8b469a903037d9cccbcc
Mohamad Rashad <m.rashad.sm@gmail.com>
2019-03-12 23:52:58 +0200

    Pickup command used in inputhandler to called when player press on specific keyboard key

---
+++ Assets/Scripts/Utilities/PickupCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCommand : Command
+{
+    Pickup pickupScript;
+
+    public PickupCommand(GameObject MovingObject)
+    {
+        pickupScript  = MovingObject.GetComponent<Pickup>();
+        if (pickupScript == null)
+        {
+            Debug.LogError("Object Should Contains Pickup Script");
+        }
+    }
+    //Called when we press a key
+    public override void Execute()
+    {
+        StartPickObject();
+    }
+    public override void StopExecuting()
+    {
+    }
+    //Undo an old command
+    public override void Undo()
+    {
+    }
+
+    //Move the Object
+    public void StartPickObject()
+    {
+        pickupScript.PickObject();
+
+    }
+}
