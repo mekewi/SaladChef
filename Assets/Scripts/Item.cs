3a74abef645adbf8dc149d002f011927477f7f94
Mohamad Rashad <m.rashad.sm@gmail.com>
2019-03-12 23:47:21 +0200

    Item Class it is base class for Vegetable Item and also if we will add another items in future it is contains: ItemId -> GUID ItemName -> ForItemName
    It is also required in any item will pickedup by player

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public Guid ItemID = Guid.NewGuid();
+    public string ItemName;
+}
