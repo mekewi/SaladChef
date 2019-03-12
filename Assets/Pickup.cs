a7d4309356e4f084a04256749c3b9e09b2a2e942
Mohamad Rashad <m.rashad.sm@gmail.com>
2019-03-12 23:45:32 +0200

    Pickup class it is requierd in player object to allow player to pick items it is have method PickObject and it is fired from InputHandler when Pickup command fired

---
+++ Assets/Pickup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Pickup : MonoBehaviour
+{
+    public GameObject CurrentHoverObject;
+    public GameManagerSO gameManagerData;
+    [SerializeField]
+    public PickupEvent PickUpAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void PickObject() {
+        if (CurrentHoverObject)
+        {
+            Item itemToPick = CurrentHoverObject.GetComponent<Item>();
+            if (itemToPick != null)
+            {
+               PickUpAction.Invoke(itemToPick);
+            }
+            else
+            {
+                Debug.Log("Item To Pick should have Item Script");
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurrentHoverObject)
+        {
+            bool isStillHover = Vector2.Distance(transform.position, CurrentHoverObject.transform.position) < gameManagerData.DistanceToPickup.Value*2;
+            if (!isStillHover)
+            {
+                CurrentHoverObject = null;
+            }
+        }
+    }
+}
