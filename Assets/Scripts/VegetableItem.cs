def6ad539b4a5b2a87298a6479555c3551233227
Mohamad Rashad <m.rashad.sm@gmail.com>
2019-03-12 23:50:56 +0200

    VegetableItem class it is inherted from Item class and it make recast based on RaycastDirction Enum And if reycast hit object that have pickup class will add it self to currenthover variable

---
+++ Assets/Scripts/VegetableItem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VegetableItem : Item
+{
+
+    public LayerMask layerMask;
+    [SerializeField]
+    GameManagerSO gameManagerData;
+    [SerializeField]
+    MovingDirction rayCastDirction;
+    public TextMeshPro ItemNameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ItemNameText.text = ItemName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        Vector2 fwd = transform.TransformDirection(GetDirction());
+        Debug.DrawRay(transform.position, fwd * gameManagerData.DistanceToPickup.Value, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, fwd, gameManagerData.DistanceToPickup.Value, layerMask);
+        if (hit)
+        {
+            Pickup pickup = hit.collider.GetComponent<Pickup>();
+            if (pickup)
+            {
+                pickup.CurrentHoverObject = gameObject;
+            }
+        }
+    }
+
+    private Vector2 GetDirction() {
+        switch (rayCastDirction)
+        {
+            case MovingDirction.Up:
+                return Vector2.up;
+            case MovingDirction.Down:
+                return Vector2.down;
+            case MovingDirction.Left:
+                return Vector2.left;
+            case MovingDirction.Right:
+                return Vector2.right;
+            default:
+                return Vector2.left;
+        }
+
+    }
+}
