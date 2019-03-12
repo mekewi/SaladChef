dd618137e807a8e9b304f5b88ca5c847a83dc745
Mohamad Rashad <m.rashad.sm@gmail.com>
2019-03-12 23:51:57 +0200

    PlayerManager Class Handling player picked items and display item in player ui and handling action in player

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    PlayerSP playerSP;
+
+    public TextMeshPro pickedItemsName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSP.PickedItems.Clear();
+    }
+    public void PickedItem(Item PickedItem) {
+        if (playerSP.PickedItems.Count < playerSP.MaxPickedItem && !playerSP.PickedItems.Contains(PickedItem))
+        {
+            playerSP.PickedItems.Add(PickedItem);
+            pickedItemsName.text += PickedItem.ItemName + ",";
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
