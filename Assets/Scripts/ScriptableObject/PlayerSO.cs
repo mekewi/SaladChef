be143a734b39795afed400c43d243416782da574
Mohamad Rashad <m.rashad.sm@gmail.com>
2019-03-12 23:56:25 +0200

    Add Variable in player scriptable object to store max items that player can pick and list of items player currently picked

--- Assets/Scripts/ScriptableObject/PlayerSO.cs
+++ Assets/Scripts/ScriptableObject/PlayerSO.cs
@@ -7,4 +7,7 @@ public class PlayerSP : ScriptableObject
 {
     public GameObject PlayerObject;
     public FloatRefrence Speed;
+    public int MaxPickedItem;
+    public List<Item> PickedItems = new List<Item>();
+
 }
